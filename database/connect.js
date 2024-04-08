7199aa29a2086c5a4ca50f9070f8d9aace2d68cc
Abidstic <abid71@student.sust.edu>
2024-04-08 14:08:38 +0600

    create schema for db, created the full db functions

--- database/connect.js
+++ database/connect.js
@@ -1,9 +1,10 @@
 import sqlite3 from 'sqlite3';
+import createSchema from './schema.js';
 
 // Function to connect to the database
 const connectDB = () => {
     return new Promise((resolve, reject) => {
-        const db = new sqlite3.Database('./database.db', (err) => {
+        const db = new sqlite3.Database('./store.db', (err) => {
             if (err) {
                 reject(err); // Reject if there's an error
             } else {
@@ -11,7 +12,14 @@ const connectDB = () => {
                 resolve(db); // Resolve with the database object if connected successfully
             }
         });
-    });
+    })
+        .then((db) => {
+            // After connecting to the database, call createSchema function
+            return createSchema(db);
+        })
+        .catch((err) => {
+            console.error('Error connecting to the database:', err);
+        });
 };
 
 export default connectDB;
